904c7c22001ba0ed762c75557cbac889a0310d7b
Kakuho <54634665+Kakuho@users.noreply.github.com>
2021-03-04 22:41:37 +0000

    Create 1.cpp

---
+++ Chapter5/Drills/1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "std_lib_facilities.h"
+int main()
+try {
+ /* 1
+ Cout << "Success!\n";                  Error: unidentified Cout
+  */
+
+ cout << "Success!\n";
+ keep_window_open();
+ return 0;
+}
+catch (exception& e) {
+ cerr << "error: " << e.what() << '\n';
+ keep_window_open();
+ return 1;
+}
+catch (...) {
+ cerr << "Oops: unknown exception!\n";
+ keep_window_open();
+ return 2;
+}
